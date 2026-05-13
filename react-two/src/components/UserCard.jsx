89213b1cab5d546fdb655d348421dbd9ed175026
Varshitha-2509 <jinkavarshithapadmasali@gmail.com>
2026-05-13 16:52:17 +0530

    Components in react

---
+++ react-two/src/components/UserCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const UserCard = () => {
+  return (
+    <div className='user-container'>
+      <p id='user-name'>Varshitha</p>
+      <img id='user-img' src="" alt="aiml" />
+      <p id='user-desp'>Description </p>
+    </div>
+  )
+}
+
+export default UserCard
