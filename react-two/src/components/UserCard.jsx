e37305e17cdee04e300b16c8e79e6d67418bc62a
Varshitha-2509 <jinkavarshithapadmasali@gmail.com>
2026-05-14 23:36:16 +0530

    Changed lines of usercard

--- react-two/src/components/UserCard.jsx
+++ react-two/src/components/UserCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import mb from '../assets/mb.jpeg'
 
 const UserCard = () => {
   return (
     <div className='user-container'>
       <p id='user-name'>Varshitha</p>
-      <img id='user-img' src="" alt="aiml" />
+      <img id='user-img' src={mb}  alt="aiml" />
       <p id='user-desp'>Description </p>
     </div>
   )
